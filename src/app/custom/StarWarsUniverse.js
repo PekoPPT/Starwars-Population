f3816c4685ca983260cbabdeb05d90251b719292
Petar Todorov <petar.ptodorov@gmail.com>
2021-02-16 18:27:25 +0200

    change Files to File. Edit films filtering

--- src/app/custom/StarWarsUniverse.js
+++ src/app/custom/StarWarsUniverse.js
@@ -1,5 +1,5 @@
 import EventEmitter from 'eventemitter3';
-import Films from './Films';
+import Films from './Film';
 import Planet from './Planet';
 
 export default class StarWarsUniverse extends EventEmitter {
@@ -8,6 +8,7 @@ export default class StarWarsUniverse extends EventEmitter {
 
     this.films = [];
     this.planet = null;
+    this.filmUrlsSet = new Set();
   }
 
   async init() {
@@ -28,14 +29,22 @@ export default class StarWarsUniverse extends EventEmitter {
 
     await this.fetchPeoplePage(1).then((peopleData) => {
       const that = this;
-      let config = { populationDelay: 1 };
+      const config = { populationDelay: 1 };
       const planetInstance = new Planet(zeroPopulationPlanet, config, peopleData.results);
 
       this.planet = planetInstance;
       this.planet.addListener('person_born', function _onPersonBorn(filmsUrl) {
-        that.films.push(...filmsUrl.filmUrls.map(url => new Films(url)));
+        const currentSetLength = that.filmUrlsSet.size;
+
+        filmsUrl.filmUrls.forEach((url) => {
+          that.filmUrlsSet.add(url);
+        });
+        if (currentSetLength != that.filmUrlsSet.size) {
+          that.emit(StarWarsUniverse.events.FILM_ADDED);
+        }
       });
-      this.planet.addListener('populating_completed', function () {
+      this.planet.addListener('populating_completed', function _onPopulationCompleted() {
+        that.films.push([...that.filmUrlsSet].map((url) => new Films(url)));
         that.emit(StarWarsUniverse.events.UNIVERSE_POPULATED);
       });
 
